05ea551c14acba76ad22ce346fe5741f63a16980
Oluwaseun <adunmofestus@gmail.com>
2023-10-14 18:49:32 +0100

    al

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-// const searchForm = document.getElementById('search-form');
-// const searchBox = document.getElementById('search-box');
-// const searchResult = document.getElementById('search-result');
-// const showMoreBtn = document.getElementById('show-more-btn');
-
-// let keyword = "";
-// let page = 1;
-
-// async function searchImages(){
-//     keyword = searchBox.value;
-// const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=O2ovLYkYrLJpv4y60ZXqBxy_pIXrFru5m-gae6wyh-k';
-//     const response = await fetch(url);
-//     const data = await response.json();
-
-//     const results = data.results;
-    
-//     results.map((result) =>{
-//         const image = document.createElement("img");
-//         image.src = result.urls.small;
-//         const imageLink = document.createElement("a");
-//         imageLink.href = result.links.html;
-//         imageLink.target = "_blank";
-
-//         imageLink.appendChild(image);
-//         searchResult.appendChild(imageLink);
-//     })
-// }
-
-// searchForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     page = 1;
-//     searchImages();
-// })
-
-// // console.log('Happy new day');
-
-// // fetch("https://api.unsplash.com/search/photos?page=1&query=search.value&client_id=${accesskey}").then(function (res ){
-// //     console.log(res.json())
-// // })
 
 const searchForm = document.getElementById('search-form');
 const searchBox = document.getElementById('search-box');
@@ -55,6 +16,7 @@ async function searchImages() {
 
     if(page === 1){
         searchResult.innerHTML = '';
+        displayedImages = 0;
     }
 
     const results = data.results;
@@ -74,7 +36,7 @@ async function searchImages() {
         }
     });
     // Update the display of the "Show more" button based on the limit
-    if (displayedImages >= 5) {
+    if (displayedImages > 1) {
         showMoreBtn.style.display = 'block'; // Hide the button
     } else {
         showMoreBtn.style.display = 'none'; // Show the button
@@ -84,7 +46,7 @@ async function searchImages() {
 searchForm.addEventListener("submit", function(e) {
     e.preventDefault();
     page = 1;
-    displayedImages = 0; // Reset the displayed image count
+    // displayedImages = ++; // Reset the displayed image count
     searchImages();
 });
 
